938f52b267e17a799089cdb599386fc192d2e6bf
Luiz Henrique <lhcontato2020@gmail.com>
2025-06-22 19:47:57 -0300

    feat: add authenticate method to AuthController

--- src/main/java/com/henr/colab_prefeitura/modules/users/controllers/AuthController.java
+++ src/main/java/com/henr/colab_prefeitura/modules/users/controllers/AuthController.java
@@ -8,8 +8,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.henr.colab_prefeitura.modules.users.dtos.AuthenticateUserRequestDTO;
+import com.henr.colab_prefeitura.modules.users.dtos.AuthenticateUserResponseDTO;
 import com.henr.colab_prefeitura.modules.users.dtos.RegisterUserRequestDTO;
 import com.henr.colab_prefeitura.modules.users.dtos.RegisterUserResponseDTO;
+import com.henr.colab_prefeitura.modules.users.useCases.AuthenticateUserUseCase;
 import com.henr.colab_prefeitura.modules.users.useCases.RegisterUserUseCase;
 
 import jakarta.validation.Valid;
@@ -21,9 +24,18 @@ public class AuthController {
     @Autowired
     private RegisterUserUseCase registerUserUseCase;
 
+    @Autowired
+    private AuthenticateUserUseCase authenticateUserUseCase;
+
     @PostMapping("/register")
     public ResponseEntity<RegisterUserResponseDTO> register(@Valid @RequestBody RegisterUserRequestDTO dto) {
         var result = this.registerUserUseCase.execute(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
+
+    @PostMapping("/sessions")
+    public ResponseEntity<AuthenticateUserResponseDTO> authenticate(@Valid @RequestBody AuthenticateUserRequestDTO dto) {
+        var result = this.authenticateUserUseCase.execute(dto);
+        return ResponseEntity.ok().body(result);
     }
 }
