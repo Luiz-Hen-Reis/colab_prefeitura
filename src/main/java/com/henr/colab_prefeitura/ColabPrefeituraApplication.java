c2e5be3d8a860d6b167cbd3a254be6459d36fb7b
Luiz Henrique <lhcontato2020@gmail.com>
2025-06-22 10:50:52 -0300

    initial setup

---
+++ src/main/java/com/henr/colab_prefeitura/ColabPrefeituraApplication.java
@@ -0,0 +1,13 @@
+package com.henr.colab_prefeitura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColabPrefeituraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColabPrefeituraApplication.class, args);
+	}
+
+}
