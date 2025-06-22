87bb544c80b615e6a816193f361df3a988fc07e6
Luiz Henrique <lhcontato2020@gmail.com>
2025-06-22 11:48:45 -0300

    feat: add findByEmailOrCPF method in UserRepository

--- src/main/java/com/henr/colab_prefeitura/modules/users/repositories/UserRepository.java
+++ src/main/java/com/henr/colab_prefeitura/modules/users/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package com.henr.colab_prefeitura.modules.users.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.henr.colab_prefeitura.modules.users.entities.User;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-    
+    Optional<User> findByEmailOrCPF(String email, String CPF);
 }
