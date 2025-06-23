484490d0883d3ab65cdea11d347b90ca256622eb
Luiz Henrique <lhcontato2020@gmail.com>
2025-06-23 18:35:21 -0300

    feat: add findAllByUserId method in OccurrenceRepository

--- src/main/java/com/henr/colab_prefeitura/modules/occurrences/repositories/OccurrenceRepository.java
+++ src/main/java/com/henr/colab_prefeitura/modules/occurrences/repositories/OccurrenceRepository.java
@@ -1,10 +1,12 @@
 package com.henr.colab_prefeitura.modules.occurrences.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.henr.colab_prefeitura.modules.occurrences.entities.Occurrence;
 
-public interface OccurrenceRepository extends JpaRepository<Occurrence, UUID> {   
+public interface OccurrenceRepository extends JpaRepository<Occurrence, UUID> {
+    List<Occurrence> findAllByUserId(UUID userId);   
 }
