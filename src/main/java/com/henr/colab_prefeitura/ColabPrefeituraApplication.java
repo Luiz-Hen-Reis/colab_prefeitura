e43b02da112f230e6e9152374357aed7a7ce29a2
Luiz Henrique <lhcontato2020@gmail.com>
2025-06-24 17:58:11 -0300

    chore: add title, version and description of the API to docs

--- src/main/java/com/henr/colab_prefeitura/ColabPrefeituraApplication.java
+++ src/main/java/com/henr/colab_prefeitura/ColabPrefeituraApplication.java
@@ -3,7 +3,18 @@ package com.henr.colab_prefeitura;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(
+		title = "API de Ocorrências Urbanas (Colab Prefeitura)",
+		version = "1.0.0",
+		description = "API REST para gerenciamento de ocorrências urbanas, permitindo que cidadãos reportem problemas como buracos, iluminação pública, coleta de lixo e outros serviços urbanos. Fornece endpoints para criação, consulta, atualização e exclusão das ocorrências, além de funcionalidades para integração com sistemas municipais."
+	)
+)
+
 public class ColabPrefeituraApplication {
 
 	public static void main(String[] args) {
